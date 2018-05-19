79247bf4e40001ef82d91733c3bbd7bfe801e270
lorenzwalthert <lorenz.walthert@icloud.com>
2018-05-20 01:31:37 +0200

    adding answer count.

--- competition/xbg.R
+++ competition/xbg.R
@@ -3,10 +3,11 @@ library(magrittr)
 all <- fst::read_fst(here::here("competition/01-data/cleaned"))
 
 train <- all %>%
-  select(-contains("text"))
+  select(-contains("text")) %>%
+  na.omit()
 
 small_train <- train %>%
-  sample_frac(0.8)
+  sample_frac(0.1)
 
 
 library(recipes)
@@ -29,29 +30,28 @@ trainTask <- makeRegrTask(data = small_train_prep, target = "answer_score")
 xgb_learner <- makeLearner("regr.xgboost", nrounds = 10)
 
 # Create a model
-
-xgb_model <- train(xgb_learner, task = trainTask)
-
-
-model_Params <- makeParamSet(
-  makeDiscreteParam("nrounds", c(2, 3)),
-  makeDiscreteParam("max_depth", c(16, 18, 20)),
-  makeDiscreteParam("min_child_weight", c(01., 0.3, 0.5)),
-  makeDiscreteParam("lambda", c(2.4, 2.8))
-  #makeNumericParam("eta", lower = 0.001, upper = 0.5)
-  # makeNumericParam("subsample", lower = 0.10, upper = 0.80),
-  
-  # makeNumericParam("colsample_bytree", lower = 0.2, upper = 0.8)
-)
-
-parallelMap::parallelStartSocket(4)
-
 cv_folds <- makeResampleDesc("CV", iters = 3) # 3 fold cross validation
 
 rmsle <- function(task, model, pred, feats, extra.args) {
   MLmetrics::RMSLE(pmax(0, pred$data$response), pmax(0, pred$data$truth))
 }
 rmsle <- makeMeasure("rmsle", minimize = TRUE, fun = rmsle, properties = c("regr", "response"))
+
+
+
+xgb_model <- train(xgb_learner, task = trainTask)
+model_Params <- makeParamSet(
+  makeDiscreteParam("nrounds", c(4, 5)),
+  makeDiscreteParam("max_depth", c(16, 18)),
+  #makeDiscreteParam("min_child_weight", c(01., 0.3, 0.5)),
+  #makeDiscreteParam("lambda", c(2.4, 2.8))
+  makeDiscreteParam("eta", c(0.2))
+  # makeNumericParam("subsample", lower = 0.10, upper = 0.80),
+  
+  # makeNumericParam("colsample_bytree", lower = 0.2, upper = 0.8)
+)
+
+parallelMap::parallelStartSocket(4)
 
 library(MLmetrics)
 tuned_model <- tuneParams(
@@ -76,9 +76,8 @@ plotHyperParsEffect(data, x = "nrounds", y = "rmse.test.rmse", partial.dep.learn
 trainTask <- makeRegrTask(data = train_prep, target = "answer_score")
 
 xgb_learner <- makeLearner("regr.xgboost", 
-  nrounds = 3, max_depth = 20, 
-  min_child_weight = 0.5,
-  lambda = 2.8
+  nrounds = tuned_model$x$nrounds, max_depth =  tuned_model$x$max_depth, 
+  eta = tuned_model$x$eta
 )
 
 # Create a model
@@ -87,9 +86,24 @@ xgb_model <- train(xgb_learner, task = trainTask)
 
 
 ##  ............................................................................
-all <- fst::read_fst(here::here("competition/01-data/cleaned-test")) %>%
+test <- fst::read_fst(here::here("competition/01-data/cleaned-test")) %>%
   select(-contains("text"))
 
-all <- prep(prep, all)
+all <- bake(prep, test)
 
-predict(xgb_model)
+prediction <- predict(xgb_model, newdata = all) %>%
+  as.data.frame()
+prediction$answer_score <- prediction$response
+prediction$id <- test$id
+prediction$answer_score[prediction$answer_score < 1] <- 1
+
+leak <- read_csv("competition/leaked_records.csv")
+
+prediction <- bind_rows(
+  leak, 
+  prediction %>%
+    filter(!(id %in% leak$id))
+)
+
+prediction[, c("id", "answer_score")] %>%
+  write_csv("competition/xgb-four.csv")
